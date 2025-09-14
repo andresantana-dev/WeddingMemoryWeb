dd92a53f31ab38dbdafd5f826a40a007b2102a40
André Santana <andresantana.dev@gmail.com>
2025-09-14 20:54:33 +0100

    add new UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3233121790"
+    serviceWorkerVersion: "750153194"
   }
 });
